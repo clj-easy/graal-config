e16188c6de33fdfcd73ea78a16c8e99213411f2c
Karol Wójcik <karol.wojcik@tuta.io>
2021-09-23 19:07:42 +0200

    Add cheshire config

--- config/cheshire/cheshire/example/src/example/core.clj
+++ config/cheshire/cheshire/example/src/example/core.clj
@@ -1,5 +1,19 @@
 (ns example.core
+  (:require
+   [clojure.java.io :as io]
+   [cheshire.core :as json])
   (:gen-class))
 
+(def data {:hello "world"
+           ;; :date (java.util.Date. 0)
+           :world {"Hello" "world"}})
+
+
 (defn -main
-  [& _args])
+  [& _args]
+  (spit "some.json" (json/encode {:hello "world"}))
+  (json/parse-stream (io/reader "some.json"))
+  (println (-> data json/encode json/decode))
+  (println (-> data (json/encode {:pretty true}) json/decode))
+  (println (-> data (json/encode {:pretty true}) json/decode))
+  (println (-> data (json/encode {:pretty true}) ((fn [x] (json/decode x true))))))
