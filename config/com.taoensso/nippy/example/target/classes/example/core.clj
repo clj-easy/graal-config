5b51865b2071e7ca38ca6d32719ae694002c4307
Karol Wójcik <karol.wojcik@tuta.io>
2021-09-23 10:56:56 +0200

    Correct the configuration for nippy

--- config/com.taoensso/nippy/example/target/classes/example/core.clj
+++
@@ -1,9 +0,0 @@
-(ns example.core
-  (:require
-   [taoensso.nippy :as nippy])
-  (:gen-class))
-
-(defn -main
-  [& args]
-  (prn (into [] (nippy/freeze nippy/stress-data)))
-  (prn (nippy/thaw (nippy/freeze nippy/stress-data))))
