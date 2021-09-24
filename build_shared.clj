b275622e891a15c66789cdf01c71a9b4665af530
Michiel Borkent <michielborkent@gmail.com>
2021-09-24 11:01:19 +0200

    Command line parsing

--- build_shared.clj
+++ build_shared.clj
@@ -1,12 +1,22 @@
 (ns build-shared)
 
-(def args (apply hash-map *command-line-args*))
+;;;; Command line parsing
 
-(doseq [[arg-name arg-val] args]
+;; This processes command line args like
+;;
+;; $ bb native-image-test :dir config/com.h2database/h2/example :graalvm-version 21.2.0
+;;
+;; and defines a var for each command line arg to be used within tasks.
+
+(doseq [[arg-name arg-val] (partition 2 *command-line-args*)]
   (intern *ns* (symbol (subs arg-name 1)) arg-val))
+
+;; Helper to define defaults for optional arguments
 
 (defn def-default [sym default]
   (when-not (resolve sym)
     (intern *ns* sym default)))
 
 (def-default 'graalvm-version "21.1.0")
+
+;;;; End command line parsing
