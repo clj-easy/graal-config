51a5d3fd15bd7830bdadfc87b051a78a5502593d
Karol Wójcik <karol.wojcik@tuta.io>
2021-09-28 10:19:22 +0200

    clj-test-containers runtime error

--- recipe/example/src/example/core.clj
+++ recipe/example/src/example/core.clj
@@ -1,5 +1,8 @@
 (ns example.core
+  (:require [net.cgrand.xforms :as x])
   (:gen-class))
 
 (defn -main
-  [& _args])
+  [& _args]
+  (println (sequence (x/partition 4 (x/reduce +)) (range 16)))
+  )
